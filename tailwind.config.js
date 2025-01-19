691153641609e02b54a22e4d132d32d2a1ee70fa
Chirag <Khanna.Chirag@bcg.com>
2025-01-19 21:26:43 +0530

    feat: add tailwind, material css

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    prefix: 'dc--',
+    important: true,
+    content: ['./src/**/*.{html,ts}'],
+    theme: {
+      extend: {
+        container: {
+          center: true,
+        },
+        colors: {
+          primary: '#7d01fa',
+          secondary: '#e8e1f7',
+          accent: '#0dbfa9',
+        },
+      },
+    },
+    plugins: [],
+  };
