b5acb892c2b9397c32ee0c8c0489c964f5a90ba6
Chirag <Khanna.Chirag@bcg.com>
2025-01-20 21:28:27 +0530

    feat: create register and login component

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { combineLatest } from 'rxjs';
+
+import { AuthFacade } from '../../../core/auth/store/auth.facade';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  private readonly authFacade = inject(AuthFacade);
 
+  readonly registerForm = new FormGroup({
+    firstName: new FormControl('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+    lastName: new FormControl('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+    email: new FormControl('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+  });
+
+  readonly vm$ = combineLatest({
+    isLoading: this.authFacade.isLoadingLogin$,
+    showLoginError: this.authFacade.hasLoginError$,
+  });
+
+  submit() {
+    const { email, password, firstName, lastName } = this.registerForm.value;
+    this.authFacade.register(
+      email as string,
+      password as string,
+      firstName as string,
+      lastName as string
+    );
+  }
 }
